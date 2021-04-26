f4c405dd72dd47c52417d64a247d152f2cced9fb
Mihir Sanghavi <mihir.h.sanghavi@gmail.com>
2021-04-26 09:37:22 -0400

    Add optimal pow function

--- script.js
+++ script.js
@@ -19,3 +19,14 @@ function merge(left, right) {
     }
     return [...result, ...left, ...right] // left over elements in left or right
 }
+
+var myPow = function(x, n) {
+    if (n < 0) // -ve exponents
+        return 1 / myPow(x, -n)
+    if (n >= 0 && n < 1) // no int division
+        return 1
+    if (n % 2 < 1)
+        return myPow(x * x, n / 2)
+    else 
+        return x * myPow(x * x, n / 2)
+};
